a1a99449276d1cf39ac83729512478af715fcfcc
Wellington Ferreira da Silva <108769429+Wellingt0ndev@users.noreply.github.com>
2022-12-20 15:21:09 -0300

    Add duas soluções em js

--- calculaPorcentagemSorteio/src/Main.java
+++ calculaPorcentagemSorteio/src/Main.java
@@ -75,22 +75,3 @@ Output:
     }
 }
 
-/* JAVASCRIPT
-  function calculaPorcentagemSorteio(assinante, minutosAssistidos) {
-  const horas = minutosAssistidos.map((item, index) => {
-            // Transforma em horas e arredonda.
-    const arredondaHoras = Math.ceil(item / 60);
-            return assinante[index] ? arrendondaHoras * 2 : arredondaHoras;
-        });
-
-        // Soma todas as horas de assinante arredondadas com bônus incluído
-  const totalHoras = horas.reduce(
-                (valorAnterior, valorAtual) => valorAnterior + valorAtual
-  );
-
-        // Gera um array com o percentual arredondado
-        return horas.map(item => Math.round((item / totalHoras) * 100));
-
-    }*/
-
-
--- calculaTopOcorrenciasDeQueries/src/Main.java
+++ calculaTopOcorrenciasDeQueries/src/Main.java
@@ -46,21 +46,3 @@ Obs: Quando houver queries com quantidades iguais, priorizar o retorno de acordo
     }
 }
 
-/*
-function calculaTopOcorrenciasDeQueries(texto,queries,k) {
-  //Pega os termos e itera
-  const queryOcurrences = queries.map(querie => {
-  //Expressão regular serve para criar uma chave de identificação
-  const regex = new RegExp(querie, 'g');
-  //Atribui os match para as ocorrencias dentro do texto
-  const res = texto.match(regex);
-  //retorna um objeto com o termo e a quantidade de vezes que encontrou no texto
-     return { querie, length: res.length };
-  });
-  //Faz a ordenação pelo tamanho do maior para o menor
-  const sortedQueryOcurrences = queryOcurrences.sort((a, b) => b.length - a.length);
-  //retorna o array de termos ordenados, corta o array de acordo com o parametro k e itera com o valor desejado
-  return sortedQueryOcurrences.slice(0, k).map(item => item.querie);
-}*/
-
-
